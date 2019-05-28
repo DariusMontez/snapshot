97b8f8fc8530b463ee198d5f4c69b3735f9207f5
Drew Meier <darius.montez@gmail.com>
2019-05-27 21:38:40 -0400

    Start with template progressive web app (PWA)

---
+++ PWA/server.js
@@ -0,0 +1,32 @@
+/* 
+ * DEVELOPMENT SERVER
+ * ==============
+ * Usage
+ * > node server.js
+ * > PORT=6000 node server.js
+ */
+
+const express = require(`express`);
+const app = express();
+
+app.use(express.static(`public`));
+
+// log requests
+// app.use(function(req, res, next) {
+//     console.log(`${req.method} ${req.path}`);
+//     next(null);
+// });
+
+// log errors
+// app.use(function(err, req, res, next) {
+//     console.error(`${err}`);
+//     next(err);
+// });
+
+// start server on specified port
+const port = process.env.PORT || 3000;
+const server = app.listen(port, function() {
+    console.log(`Development server started on port ${port}`);
+});
+
+module.exports = server;
