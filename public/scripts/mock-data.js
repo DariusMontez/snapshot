2de973df80ab8ba3aa758260323fcd4e85af99c6
Drew Meier <darius.montez@gmail.com>
2019-05-30 16:15:33 -0400

    Improve PWA lighthouse score. fix several meta-tag issues

--- public/scripts/mock-data.js
+++ public/scripts/mock-data.js
@@ -1,13 +1,13 @@
 let data = {
     teams: [
-        {name: "Will + Joe", photoURL: "http://lorempixel.com/48/48"},
-        {name: "Sam + Bob", photoURL: "http://lorempixel.com/48/48"},
-        {name: "Rob + Jef", photoURL: "http://lorempixel.com/48/48"},
-        {name: "Tom + Sue", photoURL: "http://lorempixel.com/48/48"},
-        {name: "Terry + Pam", photoURL: "http://lorempixel.com/48/48"},
-        {name: "Mac + Mat", photoURL: "http://lorempixel.com/48/48"},
-        {name: "Claire + Jim", photoURL: "http://lorempixel.com/48/48"},
-        {name: "Riley + Josh", photoURL: "http://lorempixel.com/48/48"},
+        {name: "Will + Joe", photoURL: "https://lorempixel.com/48/48"},
+        {name: "Sam + Bob", photoURL: "https://lorempixel.com/48/48"},
+        {name: "Rob + Jef", photoURL: "https://lorempixel.com/48/48"},
+        {name: "Tom + Sue", photoURL: "https://lorempixel.com/48/48"},
+        {name: "Terry + Pam", photoURL: "https://lorempixel.com/48/48"},
+        {name: "Mac + Mat", photoURL: "https://lorempixel.com/48/48"},
+        {name: "Claire + Jim", photoURL: "https://lorempixel.com/48/48"},
+        {name: "Riley + Josh", photoURL: "https://lorempixel.com/48/48"},
     ],
 }
 
