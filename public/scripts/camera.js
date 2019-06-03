847ac2b21636d6de060ae53cbf2d67947d8894c7
Drew Meier <darius.montez@gmail.com>
2019-06-03 16:47:56 -0400

    Save player list to localStorage

--- public/scripts/camera.js
+++ public/scripts/camera.js
@@ -1,3 +1,5 @@
+import _ from '/scripts/elements.js'
+
 /*
 Steps to use camera
 
@@ -38,77 +40,88 @@ Steps to use camera
 // CAMERA STREAMING
 // ================
 
-if (!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) {
-    console.error("This browser does not support camera devices");
+function CameraPage() {
+    const $videoStream = _("video", {autoplay: true, id: "video-stream"});
+    const $captureButton = _("button", {id: "camera-click", "aria-label": "take photo"});
+
+    const el = _("div", {class: "page", id: "camera-page"},
+        $videoStream,
+        $captureButton,
+    );
+
+    if (!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) {
+        console.error("This browser does not support camera devices");
+    }
+
+    // const $videoStream = document.getElementById("video-stream");
+
+    window.onresize = e => {
+        $videoStream.width = document.body.offsetWidth;
+        $videoStream.height = document.body.offsetHeight;
+    };
+    window.onresize();
+
+    const medaConstraints = {
+        audio: false,
+        video: {
+            facingMode: "environment",
+            width: 1920,
+            height: 1080,
+        }
+    };
+
+    navigator.mediaDevices.getUserMedia(medaConstraints)
+        .then(stream => {
+            $videoStream.srcObject = stream;
+        })
+        .catch(err => {
+            console.warn(`User rejected permission to use camera. `);
+            // display prompt to user:
+            // "Please reload the app and give permission to use the camera."
+
+            alert("Please reload the app and give permission to use the camera.");
+        });
+
+    // IMAGE CAPTURE
+    // =============
+
+    const $captureCanvas = document.createElement("canvas");
+    const $captureSound = document.createElement("audio");
+
+    $captureSound.src = "/audio/42899__freqman__canon-dos-d30-no-focus.wav";
+
+    // const $captureButton = document.getElementById("camera-click");
+    $captureButton.onclick = e => {
+
+        // play capture sound!
+        $captureSound.play();
+
+        // draw video frame to canvas
+
+        $captureCanvas.width = $videoStream.width;
+        $captureCanvas.height = $videoStream.height;
+
+        const ctx = $captureCanvas.getContext("2d");
+        ctx.drawImage($videoStream, 0, 0);
+
+        // create image from canvas data
+        // use webp format. Older browsers will fall back to png.
+        const capturedImage = new Image();
+        capturedImage.src = $captureCanvas.toDataURL("image/webp");
+
+        // broadcast custom "capture" event with image
+
+        const captureEvent = new Event("capture");
+        captureEvent.data = {capturedImage};
+
+        $captureButton.dispatchEvent(captureEvent);
+
+
+    }
+
+    return el;
 }
 
-const $videoStream = document.getElementById("video-stream");
-
-window.onresize = e => {
-    $videoStream.width = document.body.offsetWidth;
-    $videoStream.height = document.body.offsetHeight;
-};
-window.onresize();
-
-
-
-const medaConstraints = {
-    audio: false,
-    video: {
-        facingMode: "environment",
-        // frameRate: 10,
-        // width: $videoStream.width,
-        // height: $videoStream.height,
-        width: 1920,
-        height: 1080,
-    }
-};
-
-navigator.mediaDevices.getUserMedia(medaConstraints)
-    .then(stream => {
-        $videoStream.srcObject = stream;
-    })
-    .catch(err => {
-        console.warn(`User rejected permission to use camera. `);
-        // display prompt to user:
-        // "Please reload the app and give permission to use the camera."
-
-        alert("Please reload the app and give permission to use the camera.");
-    });
-
-// IMAGE CAPTURE
-// =============
-
-const $captureCanvas = document.createElement("canvas");
-const $captureSound = document.createElement("audio");
-
-$captureSound.src = "/audio/42899__freqman__canon-dos-d30-no-focus.wav";
-
-const $captureButton = document.getElementById("camera-click");
-$captureButton.onclick = e => {
-
-    // play capture sound!
-    $captureSound.play();
-
-    // draw video frame to canvas
-
-    $captureCanvas.width = $videoStream.width;
-    $captureCanvas.height = $videoStream.height;
-
-    const ctx = $captureCanvas.getContext("2d");
-    ctx.drawImage($videoStream, 0, 0);
-
-    // create image from canvas data
-    // use webp format. Older browsers will fall back to png.
-    const capturedImage = new Image();
-    capturedImage.src = $captureCanvas.toDataURL("image/webp");
-
-    // broadcast custom "capture" event with image
-
-    const captureEvent = new Event("capture");
-    captureEvent.data = {capturedImage};
-
-    $captureButton.dispatchEvent(captureEvent);
-
-
+export {
+    CameraPage
 }
--- public/scripts/mock-data.js
+++ public/scripts/mock-data.js
@@ -4,10 +4,10 @@ let data = {
         {name: "Sam + Bob", photoURL: "https://lorempixel.com/48/48"},
         {name: "Rob + Jef", photoURL: "https://lorempixel.com/48/48"},
         {name: "Tom + Sue", photoURL: "https://lorempixel.com/48/48"},
-        // {name: "Terry + Pam", photoURL: "https://lorempixel.com/48/48"},
-        // {name: "Mac + Mat", photoURL: "https://lorempixel.com/48/48"},
-        // {name: "Claire + Jim", photoURL: "https://lorempixel.com/48/48"},
-        // {name: "Riley + Josh", photoURL: "https://lorempixel.com/48/48"},
+        {name: "Terry + Pam", photoURL: "https://lorempixel.com/48/48"},
+        {name: "Mac + Mat", photoURL: "https://lorempixel.com/48/48"},
+        {name: "Claire + Jim", photoURL: "https://lorempixel.com/48/48"},
+        {name: "Riley + Josh", photoURL: "https://lorempixel.com/48/48"},
     ],
 }
 
