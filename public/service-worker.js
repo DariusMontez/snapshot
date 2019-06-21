bb467659a206bea800eb2d5562485da22df8169f
Drew Meier <darius.montez@gmail.com>
2019-06-21 18:51:59 -0400

    Revise service worker to reduce redundancy in fetching and cachinh

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,32 +1,33 @@
-/* 
+/*
  * SERVICE WORKER
  * ==============
  * Cache resources as they are fetched.
+ *
+ * When the web page requests a resource:
+ * 1. the 'fetch' event listener is triggered
+ * 2. the content is fetched from the internet and stored in cache
+ * 3. the content is retreived from cache and given to the web page
+ *
  */
 
 const CACHE = "cache-offline-copy";
 
 async function updateCache(req) {
+  const cache = await caches.open(CACHE);
+
   try {
     const res = await fetch(req);
-    const cache = await caches.open(CACHE);
-    return cache.put(req, res);
+    const resClone = res.clone();
+    cache.put(req, res);
+    return resClone;
   } catch (e) {
-    console.warn(`[Service Worker] Network request failed: ${req.url}`);
+    console.warn(`[Service Worker] Serving content from cache: ${req.url}`, e);
+    return cache.match(req);
   }
 }
 
 self.addEventListener('fetch', function(event) {
   if (event.request.method != 'GET') return;
 
-  event.waitUntil(updateCache(event.request));
-  
-  event.respondWith(
-    fetch(event.request).catch(async function(error) {
-      console.warn(`[Service Worker] Serving content from cache: ${event.request.url}`);
-
-      const cache = await caches.open(CACHE);
-      return cache.match(event.request);
-    })
-  );
+  event.respondWith(updateCache(event.request));
 });
